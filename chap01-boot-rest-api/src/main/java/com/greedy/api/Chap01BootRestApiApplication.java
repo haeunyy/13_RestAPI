f953277d79fa3f348fa598a6291b375e0c7c6a03
haeunyy <ibbihe@gmail.com>
2023-04-27 12:30:30 +0900

    commit

---
+++ chap01-boot-rest-api/src/main/java/com/greedy/api/Chap01BootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.greedy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap01BootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap01BootRestApiApplication.class, args);
+	}
+
+}
---
+++ chap01-boot-rest-api/src/test/java/com/greedy/api/Chap01BootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greedy.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap01BootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
